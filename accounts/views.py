6333e72eb408fe899fc92651058173027a35c475
kmurata798 <kmurata798@gmail.com>
2019-12-13 19:10:14 -0800

    heroku plaes

--- accounts/views.py
+++ accounts/views.py
@@ -8,3 +8,6 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class LogInView(CreateView):
+    template_name = 'registration/login.html'
